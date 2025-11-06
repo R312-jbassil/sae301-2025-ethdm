64c9dda2ef645cbc25ab3062f7deaea68a9c9b95
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2025-11-06 10:54:24 +0100

    deploy.yml

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,7 +1,11 @@
 import PocketBase from 'pocketbase';
 
-const url = import.meta.env.PUBLIC_PB_URL ?? 'http://127.0.0.1:8090';
-const pb = new PocketBase(url);
+var path = '';
+if (import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    //localhost = machine de dev
+else 
+    path = 'http://localhost:8084'   //localhost = machine de déploiement
 
+const pb = new PocketBase(path);
 
 export default pb;
